50b6d701d5eacd0d79ed834aedd44ad2f8382c2d
swimswm <72042754+swimswm@users.noreply.github.com>
2020-10-12 15:36:41 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,3 +13,29 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+##
+## I set imput x as a matrix
+## and then set the solved value "u" as a null
+## then I changed every reference to "mean" to "inverse"
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+u <- NULL
+set <- function(y) {
+x <<- y
+u <<- NULL
+}
+get <- function() x
+setsolve <- function(inverse) u <<- inverse
+getsolve <- function() u
+list(set = set, get = get,
+setsolve = setinverse,
+getsolve = getineverse)
+}
+##
+##changed "mean" to "inverse" and "m" to "u"
+cacheSolve <- function(x, ...) {
+u <- x$getsolve()
+if(!is.null(u)) {
+message("getting catched matrix")
+return(u)
+}
+mat <- x$get()
